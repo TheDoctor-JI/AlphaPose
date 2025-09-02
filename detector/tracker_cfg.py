7745b114fca810b3c5cdb7fb1eb1def75db4c033
TheDoctor-JI <yifanchangle@163.com>
2025-09-03 03:10:59 +0800

    adam commit alphapose relative path change

--- detector/tracker_cfg.py
+++ detector/tracker_cfg.py
@@ -1,8 +1,8 @@
 from easydict import EasyDict as edict
 
 cfg = edict()
-cfg.CONFIG = '/home/grace_team/HKUST_GRACE/SocialTaskImplementation/visual_perception_manager/AlphaPose/detector/tracker/cfg/yolov3.cfg'
-cfg.WEIGHTS = '/home/grace_team/HKUST_GRACE/SocialTaskImplementation/visual_perception_manager/AlphaPose/detector/tracker/data/jde.1088x608.uncertainty.pt'
+cfg.CONFIG = '../visual_perception_manager/AlphaPose/detector/tracker/cfg/yolov3.cfg'
+cfg.WEIGHTS = '../visual_perception_manager/AlphaPose/detector/tracker/data/jde.1088x608.uncertainty.pt'
 cfg.IMG_SIZE =  (1088, 608)
 cfg.NMS_THRES =  0.6
 cfg.CONFIDENCE = 0.4
